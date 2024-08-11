fb3c4243cb9540fa70b97b65a1e83cd41bb980ab
Nevo <bitonnevo@gmail.com>
2024-08-11 21:02:39 +0300

    getUseRooms

--- src/routes/rooms-route.ts
+++ src/routes/rooms-route.ts
@@ -6,7 +6,8 @@ import {
     updateRoom,
     deleteRoom,
     addToFavorites,
-    removeFromFavorites
+    removeFromFavorites,
+    getUserRooms
 } from "../controllers/rooms-controller";
 import { verifyToken } from "../middleware/auth-middleware";
 
@@ -20,6 +21,7 @@ roomsRoutes.get("/:id", getRoomById);
 roomsRoutes.post("/", verifyToken, addRoom);
 roomsRoutes.patch("/:id", verifyToken, updateRoom);
 roomsRoutes.delete("/:id", verifyToken, deleteRoom);
+roomsRoutes.get("/user/:userId", verifyToken, getUserRooms);
 
 roomsRoutes.post("/favorite/:roomId", verifyToken, addToFavorites);
 roomsRoutes.delete("/favorite/:roomId", verifyToken, removeFromFavorites);
