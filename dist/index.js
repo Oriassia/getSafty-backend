f4e9ab4b0413c42d6731ff0b674cd5b4f4110c74
Ori assia <162992363+Oriassia@users.noreply.github.com>
2024-08-17 18:35:10 +0300

    ori

--- dist/index.js
+++ dist/index.js
@@ -3,20 +3,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.io = void 0;
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const db_1 = require("./config/db");
 const rooms_route_1 = require("./routes/rooms-route");
-const socket_io_1 = require("socket.io");
 const http_1 = __importDefault(require("http"));
-const redAlert_middleware_1 = __importDefault(require("./middleware/redAlert-middleware"));
 const app = (0, express_1.default)();
 const PORT = 3000;
 const server = http_1.default.createServer(app);
-exports.io = new socket_io_1.Server(server, {
-    cors: { origin: "*", methods: ["GET", "POST", "PATCH", "DELETE", "PUT"] },
-});
+// export const io = new Server(server, {
+//   cors: { origin: "*", methods: ["GET", "POST", "PATCH", "DELETE", "PUT"] },
+// });
 async function main() {
     // Middleware
     app.use(express_1.default.json());
@@ -28,20 +25,20 @@ async function main() {
     }));
     // Connect to database
     (0, db_1.connectDB)();
-    app.use((0, redAlert_middleware_1.default)(exports.io));
-    exports.io.on("connection", (socket) => {
-        console.log("connection", socket.id);
-        socket.on("disconnect", () => {
-            console.log("disconnect");
-        });
-    });
+    // app.use(socketMiddleware(io));
+    // io.on("connection", (socket: Socket) => {
+    //   console.log("connection", socket.id);
+    //   socket.on("disconnect", () => {
+    //     console.log("disconnect");
+    //   });
+    // });
     // Routes
     // app.use("/api/auth", authRoutes);
     app.use("/api/room", rooms_route_1.roomsRoutes);
     // app.use("/api/alert", alertRoute);
     // displaied if connected successfuly
     app.get("/", (req, res) => res.json("Express on Vercel"));
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
     });
 }
