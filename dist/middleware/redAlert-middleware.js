0e6f66020924c75d383fc17a9bb3bda366dae71b
Ori assia <162992363+Oriassia@users.noreply.github.com>
2024-09-14 11:19:26 +0300

    production update

--- dist/middleware/redAlert-middleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const socketMiddleware = (io) => (req, res, next) => {
-    req.io = io;
-    next();
-};
-exports.default = socketMiddleware;
--- dist/routes/red-alert-route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.alertRoute = void 0;
-const express_1 = require("express");
-const red_alert_controller_1 = require("../controllers/red-alert-controller");
-exports.alertRoute = (0, express_1.Router)();
-// Route to get the nearest zone based on location
-exports.alertRoute.get("/", red_alert_controller_1.poll); // Added this line
